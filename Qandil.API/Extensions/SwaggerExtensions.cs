3471239a9e50839d47976240cba727fdaf4a3c16
Rayan <rayanimo200@gmail.com>
2026-06-10 23:14:44 +0300

    UpdateProgram.cs

--- Qandil.API/Extensions/SwaggerExtensions.cs
+++ Qandil.API/Extensions/SwaggerExtensions.cs
@@ -1,30 +1,21 @@
 ﻿using Microsoft.OpenApi.Models;
-using Unchase.Swashbuckle.AspNetCore.Extensions.Extensions;
-
 
 namespace Qandil.API.Extensions
 {
-    public static  class SwaggerExtensions
+    public static class SwaggerExtensions
     {
         public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
         {
             services.AddSwaggerGen(options =>
             {
-                // Basic Info
                 options.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Title = "Qandeel WEBSITE → API Documentation",
                     Version = "v1.0.0"
                 });
 
-               // Enhancements
-               // options.EnableAnnotations();
                 options.CustomSchemaIds(type => type.ToString());
-               // options.AddEnumsWithValuesFixFilters();
-              // options.ParameterFilter<QueryArrayParamFilter>();
-               // options.IncludeXmlCommentsFromInheritDocs(true, typeof(string));
 
-                // Security Definition
                 options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
                     Description = "JWT Authorization header using the Bearer scheme. Example: 'Bearer {token}'",
@@ -34,24 +25,20 @@ namespace Qandil.API.Extensions
                     Scheme = "Bearer"
                 });
 
-                // Security Requirement
                 options.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
                 {
-                    new OpenApiSecurityScheme
                     {
-                        Reference = new OpenApiReference
+                        new OpenApiSecurityScheme
                         {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
                         },
-                        Scheme = "oauth2",
-                        Name = "Bearer",
-                        In = ParameterLocation.Header
-                    },
-                    Array.Empty<string>()
-                }
-            });
+                        Array.Empty<string>()
+                    }
+                });
             });
 
             return services;
--- Qandil.API/Program.cs
+++ Qandil.API/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSwaggerGen();
+builder.Services.AddCustomSwagger();
 builder.Services.AddCustomCors(configuration);
 builder.Services.AddAPI(configuration);
 builder.Services.AddInfrastructure(configuration);
