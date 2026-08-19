963724502b77b099811e99375e108ab0e22a741d
DESKTOP-MM59FF5\LOQ <lofi2dhrouj@gmail.com>
2026-08-19 16:27:31 +0300

    update entity relations in test

--- Qandil.Core/Entity/ChildTestSubjectMark.cs
+++ Qandil.Core/Entity/ChildTestSubjectMark.cs
@@ -5,11 +5,7 @@ namespace Qandil.Core.Entity
     public class ChildTestSubjectMark:BaseEntity
     {
         public double ObtainMark {  get; set; }
-        public int AttemptNumber {  get; set; }
-        public DateTime TakenAt { get; set; }
-        public string Notes {  get; set; }
-        public Guid TestSubjectId { get; set; }
-        public TestSubject TestSubject { get; set; }
+        public string? Notes {  get; set; }
         public Guid ChildTestId { get; set; }
         public ChildTest ChildTest { get; set; }
         public Guid SubjectId { get; set; }
--- Qandil.Core/Entity/Test.cs
+++ Qandil.Core/Entity/Test.cs
@@ -6,9 +6,7 @@ namespace Qandil.Core.Entity
     public class Test:BaseEntity
     {
         public string Name { get; set; }
-
         public string Title {  get; set; }
-        public TestType TestType { get; set; }
         public bool HasPreTest {  get; set; }
         public Level Level { get; set; }
         public Guid LevelId { get; set; }  
