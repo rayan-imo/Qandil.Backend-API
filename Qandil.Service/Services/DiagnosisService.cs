c7daf66d9ffb2ae7104bb8fb88b6343efdf11447
DESKTOP-MM59FF5\LOQ <lofi2dhrouj@gmail.com>
2026-09-05 14:53:17 +0300

    fix: use async methods and validate Child/Employee existence before adding Diagnosis

--- Qandil.Service/Services/DiagnosisService.cs
+++ Qandil.Service/Services/DiagnosisService.cs
@@ -44,10 +44,10 @@ namespace Qandil.Service.Services
         public async Task<Result<Diagnosis>> AddAsync(DiagnosisRequestDto dto)
         {
             await new DiagnosisValidator().ValidateAndThrowAsync(dto);
-            var child = _uow.ChildRepository.GetById(dto.ChildId);
+            var child = await _uow.ChildRepository.GetByIdAsync(dto.ChildId);
             if (child == null)
                 return Result<Diagnosis>.Failure("الطفل غير موجود ");
-            var employee = _uow.EmployeeRepository.GetById(dto.EmployeeId);
+            var employee =await _uow.EmployeeRepository.GetByIdAsync(dto.EmployeeId);
             if (employee == null)
                 return Result<Diagnosis>.Failure("الموظف غير موجود");
             var diagnosis = new Diagnosis
